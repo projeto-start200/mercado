713b4a8f4e0f02dc5c814792115342da61ba91b8
Gerson114 <164920308+Gerson114@users.noreply.github.com>
2024-04-27 00:16:06 -0300

    menu

---
+++ mercado/src/components/categoria.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+const categoria = () => {
+  return (
+    
+
+    <div className='compra'>
+    <div className='fundo3'>
+     <img src="macarrao.png" alt="" />
+      <div className='titulo'>
+        <h2> macarrao 68g</h2>
+      </div>
+
+      <div className='val'><h4>validade 03/5/24</h4></div>
+
+      <div className='boot'> <button>adicionar</button></div>
+      
+
+
+    </div>
+
+  </div>
+  )
+}
+
+export default categoria
